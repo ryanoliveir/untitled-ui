17334b07237c6d809f3bd7747165cd46e8458f30
ryanoliveir <ryandehallo229@gmail.com>
2026-01-26 00:22:18 -0300

    styles: responsive navbar

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Logo } from './Logo'
 import {
   Home,
@@ -9,12 +10,15 @@ import {
   LifeBuoy,
   Settings,
   Search,
+  Menu,
 } from 'lucide-react'
 
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
 import * as Input from '../Input'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Button } from '../Button'
 
 const menuItems = [
   {
@@ -56,36 +60,48 @@ const bottomItems = [
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
-      <Logo />
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 data-[state=open]:h-screen lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0">
+      <div className="flex items-center justify-between">
+        <Logo />
+        <Collapsible.Trigger asChild className="lg:hidden">
+          <Button variant="ghost">
+            <Menu className="h-6 w-6" />
+          </Button>
+        </Collapsible.Trigger>
+      </div>
 
-      <Input.Root>
-        <Input.Prefix>
-          <Search className="h-5 w-5 text-zinc-500" />
-        </Input.Prefix>
-        <Input.Control placeholder="Search" />
-      </Input.Root>
-      <nav className="space-y-0.5">
-        {menuItems.map((item) => {
-          return (
-            <NavItem key={item.title} title={item.title} icon={item.icon} />
-          )
-        })}
-      </nav>
-
-      <div className="mt-auto flex flex-col gap-6">
+      <Collapsible.Content
+        forceMount
+        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+      >
+        <Input.Root>
+          <Input.Prefix>
+            <Search className="h-5 w-5 text-zinc-500" />
+          </Input.Prefix>
+          <Input.Control placeholder="Search" />
+        </Input.Root>
         <nav className="space-y-0.5">
-          {bottomItems.map((item) => {
+          {menuItems.map((item) => {
             return (
               <NavItem key={item.title} title={item.title} icon={item.icon} />
             )
           })}
         </nav>
-        <UsedSpaceWidget />
 
-        <div className="h-px bg-zinc-200"></div>
-        <Profile />
-      </div>
-    </aside>
+        <div className="mt-auto flex flex-col gap-6">
+          <nav className="space-y-0.5">
+            {bottomItems.map((item) => {
+              return (
+                <NavItem key={item.title} title={item.title} icon={item.icon} />
+              )
+            })}
+          </nav>
+          <UsedSpaceWidget />
+
+          <div className="h-px bg-zinc-200"></div>
+          <Profile />
+        </div>
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
