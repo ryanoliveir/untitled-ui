d9f6e17ca42b69ef6557bbf413f72a49b3a1c019
ryanoliveir <ryandehallo229@gmail.com>
2026-02-01 21:36:03 -0300

    styles: fix tailwind class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en" className="dark antialiased">
       {/* <html lang="en" className="antialiased"> No dark mode */}
       <body className={inter.className}>
-        <div className="min-h-screen lg:grid lg:grid-cols-app dark:bg-zinc-900">
+        <div className="min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app">
           <Sidebar />
           <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
             {children}
